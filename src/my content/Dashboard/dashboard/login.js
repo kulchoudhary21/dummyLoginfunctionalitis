ff1544658545858d998dff1060ecf4e023e1617c
Kuldeeep Choudhary <kulchoudhary21@gmil.com>
2023-04-17 19:53:36 +0530

    Updated files and folders

--- src/my content/Dashboard/dashboard/login.js
+++ src/my content/Dashboard/dashboard/login.js
@@ -1,15 +1,22 @@
 import "../../css/login.css";
 import { Link, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
+import { useRef } from "react";
 function Login() {
   const navigate = useNavigate();
-  let keyPress = (e) => {
-    if (e.keyCode == 13) {
-      onLogin();
-    }
-  };
+  let key=useRef("");
+  let key1=useRef("");
+  // let keyPress = (e) => {
+  //   console.log()
+  // }
+  if(key1.current.keyPress==13)
+  { 
+    console.log("jkj")
+    onLogin()
+  }
 
   let onLogin = () => {
+    console.log(key1.current.keyPress==13)
     let email = document.getElementById("email").value;
     let passwd = document.getElementById("passwd").value;
     let user_data = localStorage.getItem("user_data");
@@ -40,24 +47,26 @@ function Login() {
         <h4>Login</h4>
         <input
           id="email"
+          ref={key}
           type="email"
           className="form-control inp"
           placeholder="Your email"
           autoFocus={true}
-          onKeyDown={keyPress}
+          // onKeyDown={keyPress}
         />
         <input
           id="passwd"
           type="password"
           className="form-control inp"
           placeholder="password"
-          onKeyDown={keyPress}
+          // onKeyDown={keyPress}
+          
         />
         <ToastContainer />
         {/* <span id="msg"></span> */}
         <div className="row r">
           <div className="col-6 r1">
-            <button className="btn btn-primary inp" onClick={onLogin}>
+            <button className="btn btn-primary inp" onClick={onLogin} ref={key1} value={true}>
               Submit
             </button>
           </div>
